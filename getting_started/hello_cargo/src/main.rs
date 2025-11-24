ed76091f1dbe889ec5891edb2032d352ca7ec1fa
Nataniel Dziadzia <sumplous.sumplous@gmail.com>
2025-11-24 23:41:34 +0100

    fixing the git ignore and  end of the first chapter

--- getting_started/hello_cargo/src/main.rs
+++ getting_started/hello_cargo/src/main.rs
@@ -1,3 +1,23 @@
+/*
+ * cargo new [project_name]
+ * creates new git repo with gitignore
+ * unless its part of another git repo
+ *
+ * besides uses .toml file like python
+ *
+ * there is also the cargo --init
+ * also creates the toml file
+ *
+ * compling through -> cargo build
+ * executable in a target/debug directory
+ *          OR
+ * cargo run -> compile & run
+ *
+ * cargo check -> without executable
+ *
+ * cargo build --release -> compile with optimizations
+ * executable is in a target/release directory
+*/
 fn main() {
     println!("Hello, world!");
 }
