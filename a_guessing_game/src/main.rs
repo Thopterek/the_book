40ae246951007cc8c991303231737826ec16d2e4
Nataniel Dziadzia <ndziadzi@2-E-6.42heilbronn.de>
2025-12-02 11:41:17 +0100

    finishing the game logic, added error handling and loop logic to keep everything intact

--- a_guessing_game/src/main.rs
+++ a_guessing_game/src/main.rs
@@ -18,48 +18,71 @@ use rand::Rng;
 use std::cmp::Ordering;
 
 fn main() {
+    /*
+     * particular number generator -> current thread
+     * passing the range of generated numbers to method
+     * cargo doc --open -> to show methods and functions of dependencies
+    */
+    let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("Guest the number!\nPlease input your guess.");
     /*
      * to make variable mutable you have to add mut
      * if not -> everything is being constant
      * [new_instance]::[associated_function]
      * in this instance creating empty str
+     *
+     * added the loop before hand to wrap it around
+     * loop is just an infinite while loop (while(true))
     */
-    let mut guess = String::new();
-    /*
-     * possible rewrite (without use) std::io::stdin
-     * create a new instance of std::io::Stdin
-    */
-    io::stdin()
+    loop {
+        let mut guess = String::new();
         /*
-         * appends the line into a string
-         * uses references -> same  logic:
-         *  - they have to be made mutable
-         *  - if not things are constants
+         * possible rewrite (without use) std::io::stdin
+         * create a new instance of std::io::Stdin
         */
-        .read_line(&mut guess)
+        io::stdin()
+            /*
+            * appends the line into a string
+            * uses references -> same  logic:
+            *  - they have to be made mutable
+            *  - if not things are constants
+            */
+            .read_line(&mut guess)
+            /*
+            * uses C like enum for errors
+            * return (0) == Result Ok
+            * return (23) == Result Err
+            */
+            .expect("Error: Failed to read line.");
         /*
-         * uses C like enum for errors
-         * return (0) == Result Ok
-         * return (23) == Result Err
+         * shadowing the value of last guess -> used when converting values
+         * u32 -> unsigned 32 bit number, i32 would be normal one
+         * 
+         * added invalid input handling through Ok and Err
+         * expect line is being switched to the match
+         * before -> let guess: u32 = guess.trim().parse().expect("Please type a number!");
         */
-        .expect("Error: Failed to read line.");
-    // shadowing the value of last guess -> used when converting values
-    // u32 -> unsigned 32 bit number, i32 would be normal one
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-    // particular number generator -> current thread
-    // passing the range of generated numbers to method
-    // cargo doc --open -> to show methods and functions of dependencies
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-    // match expression allows to check enum what cmp returned
-    // its made out of arms and checks the particular pattern
-    match guess.cmp(&secret_number) {
-        // Ordering is another enum variant
-        // cmp is a method to compare with referenced value
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        /*
+         * match expression allows to check enum what cmp returned
+         * its made out of arms and checks the particular pattern
+        */
+        match guess.cmp(&secret_number) {
+            /*
+             * Ordering is another enum variant
+             * cmp is a method to compare with referenced value
+            */
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+        println!("You guessed: {guess}");
     }
-    println!("Secret number is -> {secret_number}");
-    println!("You guessed: {guess}");
 }
+
