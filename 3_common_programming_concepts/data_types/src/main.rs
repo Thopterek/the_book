4823019c2b50b56bf4f3b5f11436eb98709fdc10
Nataniel Dziadzia <sumplous.sumplous@gmail.com>
2025-12-03 19:49:43 +0100

    data types more into the possible handlings of overflows

--- 3_common_programming_concepts/data_types/src/main.rs
+++ 3_common_programming_concepts/data_types/src/main.rs
@@ -22,6 +22,7 @@ fn main() {
      * Different types of ints are based on the possible size
      * setting from unsigned to standard by i or u prefix
      * 8, 16, 32, 64, 128 bit and till isize / usize
+     * default goes to the i32
     */
     let eight: i8 = 127;
     let sixtee: i16 = 32767;
@@ -29,4 +30,38 @@ fn main() {
     let un_eight: u8 = 255;
     let un_sixtee: u16 = 65535;
     println!("for unsigned goes from 8 bit -> {un_eight}, then 16 bit -> {un_sixtee} and so on");
+    /*
+     * You can write numbers with _ as per 8_0_8
+     * to have a visual seperation and takes in formts:
+     * Decimal, Hex, Octal, Binary, Byte
+    */
+    let dec: i32 = 8_0_8;
+    let hex: i32 = 0xff;
+    println!("Dec is {dec}, Hex is {hex}, printed as decimals");
+    /*
+     * There ways to check for the overflows
+     * as per handling of it rather than expecing wrap
+     * checked fails with the explicit type
+     * overflowing returns value and boolean
+    */
+    let new: i8 = eight.wrapping_add(1);
+    println!("Overflow but wrapped_add -> {new}");
+    {
+        let test = eight.checked_add(1);
+        println!("Now with checked_add is {:?}", test);
+    }
+    let (value, state) = eight.overflowing_add(1);
+    println!("Value -> {} & State -> {}", value, state);
+    {
+        let test = eight.saturating_add(1);
+        println!("Saturating add -> {test}");
+    }
+    /*
+     * Floating point numbers are always signed
+     * base one is f64 but there is also f32
+     * same C++ standard IEEE754
+    */
+    let x = 2.0;
+    let y: f32 = 3.0;
+    println!("Different floats addition {}", x + y);
 }
