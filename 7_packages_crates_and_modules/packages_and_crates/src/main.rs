032cf578dea473f42381690955047472b28d4e78
Thopterek <nataniel.dziadzia@gmail.com>
2025-12-17 12:33:34 +0100

    starting to learn more about organising of Rust projects

---
+++ 7_packages_crates_and_modules/packages_and_crates/src/main.rs
@@ -0,0 +1,10 @@
+/*
+ * Binary crates are programs (main)
+ * Library crates are just C libs (no starting point)
+ * Crate root is a source file that Rust compiler starts from
+ * Package is a bundle of crates at most one Library and as many Binary
+*/
+
+fn main() {
+    println!("Hello, world!");
+}
